8e5b13e567b4c627a5bf0ac4c93cca279d064201
Luis Fernandez <dropmeaword@gmail.com>
2018-01-28 16:36:51 +0100

    rnamed steps and removed redundant files

---
+++ workbench/1_webserver.py
@@ -0,0 +1,43 @@
+import network
+import socket
+import time
+
+# set our access point up
+ap = network.WLAN(network.AP_IF)
+ap.active(True)
+ap.config(essid="MONKEY_ISLAND_0", authmode=network.AUTH_OPEN)
+
+# we open a listening socket and bind it on port 80 (HTTP)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind( ('0.0.0.0', 80) )
+s.listen(0)
+
+print("I am listening now...")
+
+# wait forever
+while True:
+	# if an incoming connection arrives, deal with it
+	conn, addr = s.accept()
+	print( "Got a connection from {0}".format(str(addr)) )
+
+	# read the contents of the request
+	request = conn.recv(4096)
+	# print the icoming request
+	request = str(request)
+	print("Our visitor requested this: ")
+	print(request)
+	# send back to the browser the success HTTP headers
+	conn.sendall('HTTP/1.1 200 OK\n')
+	conn.sendall('Connection: close\n')
+	conn.sendall('Server: smugglersrv\n')
+	conn.sendall('Content-Type: text/html\n\n')
+
+	# send back to the browser something to render
+	conn.sendall("My name is Janus")
+	conn.sendall("\n")
+
+	conn.close()
+
+	# we sleep for a little bit
+	time.sleep(0.01)
--- workbench/countdracula.py
+++ workbench/countdracula.py
@@ -1,10 +1,11 @@
 import time
 
-print("Hello! See me count: ")
+print("=+="*20)
+print("Hi! I will start counting now: ")
 
-i = 1
+i = 1 # start at one
 
 while True:
-	print(i)
+	print("{0} dracula".format(i))
 	i += 1
 	time.sleep(0.5)
